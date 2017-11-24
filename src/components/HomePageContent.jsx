10138e1ecdcc560e6dc130b473af041bae9c2e3a
albertadevor <alberta.devor@gmail.com>
2017-11-24 00:31:06 -0500

    added homepage content

--- src/components/HomePageContent.jsx
+++ src/components/HomePageContent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../css/HomePageContent.css';
 import { Carousel } from 'react-bootstrap';
+import projects_img from '../img/projects_header.png';
+import material_listing_img from '../img/material_listing.png';
 
 /*
  represents the home page content of the site
@@ -10,38 +12,43 @@ class HomePageContent extends Component {
     render() {
 	    return (
 		   <div id="homePageContent">
-                <h2> Welcome to ArchIndex, </h2>
-                <Carousel id="headerCaro" 
-                    nextIcon="" prevIcon="" indicators={false} interval={2000}
-                    controls={false}> 
+                <div id="caroText">
+                    <h2> Welcome to ArchIndex, </h2>
+                    <Carousel id="headerCaro" 
+                        nextIcon="" prevIcon="" indicators={false} interval={2000}
+                        controls={false}> 
 
-                    <Carousel.Item>
-                        <h2 className="blueTitle"> Maker </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="blueTitle"> Maker </h2>
+                        </Carousel.Item>
 
-                    <Carousel.Item>
-                        <h2 className="redTitle"> Dreamer </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="redTitle"> Dreamer </h2>
+                        </Carousel.Item>
 
-                    <Carousel.Item>
-                        <h2 className="purpleTitle"> Crafter </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="purpleTitle"> Crafter </h2>
+                        </Carousel.Item>
 
-                    <Carousel.Item>
-                        <h2 className="blueTitle"> Designer </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="blueTitle"> Designer </h2>
+                        </Carousel.Item>
 
-                    <Carousel.Item>
-                        <h2 className="redTitle"> Creator </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="redTitle"> Creator </h2>
+                        </Carousel.Item>
 
-                    <Carousel.Item>
-                        <h2 className="purpleTitle"> Visionary </h2>
-                    </Carousel.Item>
+                        <Carousel.Item>
+                            <h2 className="purpleTitle"> Visionary </h2>
+                        </Carousel.Item>
 
-                </Carousel>
+                    </Carousel>
+                </div>
 
-                <p onClick={() => this.props.changePage("product")}> click me! </p>
+                <img src={material_listing_img} width="70%" 
+                    onClick={() => this.props.changePage("product")}/>
+                <img src={projects_img} width="100%" 
+                    onClick={() => this.props.changePage("product")}/>
             </div>
 
 
